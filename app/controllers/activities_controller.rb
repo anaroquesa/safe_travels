528672406e770a634c71d2a0bb1d20238c97844c
Esther Mueni <emueni97@gmail.com>
2023-06-07 11:54:38 +0200

    added the contoller methods and views

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,46 @@
 class ActivitiesController < ApplicationController
+  def index
+    @activities = Activity.all
+  end
+
+  def show
+    @activity = Activity.find(params[:id])
+  end
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+    @activity.itinerary = @itinerary
+
+    if @activity.save
+      redirect_to itinerary_path alert: "Your activity has been saved."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @activity = Activity.find(params[:id])
+  end
+
+  def update
+    @activity = Activity.find(params[:id])
+    @activity.update(activity_params)
+    redirect_to activities_path(@activity)
+  end
+
+  def destroy
+    @activity = Activity.find(params[:id])
+    @activity.destroy
+    redirect_to activities_path, alert: 'Activity was successfully deleted.'
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:title, :category, :address)
+  end
 end
