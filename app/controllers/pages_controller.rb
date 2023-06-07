f9fffd78e0cdf650cd1c238559f4b0153898a119
Emily Yetsko <emilyyetsko@gmail.com>
2023-06-07 11:49:47 +0200

    added cities controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  # skip_before_action :authenticate_user!, only: [ :home, :cities ]
 
   def home
   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+City.create(name: "Berlin", country: "Germany", image_url: "https://cdn.getyourguide.com/img/location/e7cd5b184985f010.jpeg/75.jpg" )
+City.create(name: "New York City", country: "US", image_url: "https://www.travelandleisure.com/thmb/91pb8LbDAUwUN_11wATYjx5oF8Q=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/new-york-city-evening-NYCTG0221-52492d6ccab44f328a1c89f41ac02aea.jpg" )
+Activity.create(city_id: "1", title: "Brandenburg Gate", category: "Culture", image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Brandenburger_Tor_abends.jpg/1200px-Brandenburger_Tor_abends.jpg")
+Activity.create(city_id: "1", title: "Reichstag Building", category: "Culture", image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d8/Reichstag_Dome_2019.jpg")
+Activity.create(city_id: "1", title: "East Side Gallery", category: "Art", image_url: "https://images.musement.com/cover/0002/65/east-side-gallery-berlin_header-164457.jpeg")
+Activity.create(city_id: "2", title: "Empire State Building", category: "Culture", image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg/640px-Empire_State_Building_from_the_Top_of_the_Rock.jpg")
+Activity.create(city_id: "2", title: "Natural History Museum", category: "Museum", image_url: "https://image.newyorkcity.de/wp-content/uploads/2021/01/American-Museum-of-Natural-History-in-New-York-T-Rex.jpg.webp")
+Activity.create(city_id: "2", title: "Central Park", category: "Nature", image_url: "https://img.freepik.com/fotos-kostenlos/central-park-in-manhattan-new-york-ein-riesiger-schoener-park-umgeben-von-wolkenkratzern-mit-teich_181624-50335.jpg")
