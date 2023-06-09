ab0707b3496afbf68f4fe7f9dab8ed71cda5f9ba
anasa <aroquesa@gmail.com>
2023-06-09 09:11:06 +0200

    started not finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_154619) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_08_153417) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_154619) do
     t.string "image_url_three"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
   end
 
   create_table "it_activities", force: :cascade do |t|
