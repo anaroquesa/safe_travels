ace04e537d7ff80e9c67cc72d691fe6e0b04a5f2
Esther Mueni <emueni97@gmail.com>
2023-06-12 18:12:28 +0200

    doing messages and chatrooms

--- db/schema.rb
+++ db/schema.rb
@@ -80,7 +80,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_082723) do
     t.string "image_url_one"
     t.string "image_url_two"
     t.string "image_url_three"
-    t.integer "safety_rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "latitude"
