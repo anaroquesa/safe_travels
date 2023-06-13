4e52bf421c0b4afdc9075513f2d6270e92ded6d7
Dabin Im <dabinim@gmail.com>
2023-06-13 16:42:32 +0200

    all cloudinary images in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,7 +33,7 @@ City.create!(
   name: "Berlin",
   country: "Germany",
   image_url_one: "https://res.cloudinary.com/doxaalafc/image/upload/v1686586893/SafeTravels%20app/Images/City%20images/Berlin/City_Berlin_Germany_dxdavb.png",
-  image_url_two: "https://res.cloudinary.com/doxaalafc/image/upload/v1686586889/SafeTravels%20app/Images/City%20images/Berlin/Activities_2_Berlin_Germany_oniqwb.png",
+  image_url_two: "https://res.cloudinary.com/doxaalafc/image/upload/v1686653013/SafeTravels%20app/Images/City%20images/Berlin/Activities_2_Berlin_Germany_yaxnwx.png",
   image_url_three: "https://res.cloudinary.com/doxaalafc/image/upload/v1686586891/SafeTravels%20app/Images/City%20images/Berlin/Activities_3_Berlin_Germany_oaonrz.png",
   latitude: 13.381777,
   longitude: 52.531677,
@@ -125,6 +125,32 @@ City.create!(
 
 puts "Creating Activities.."
 
+# 3 for each category: Nature, Culture, Art, Food
+# BERLIN
+Activity.create!(
+  city_id: "1",
+  title: "Tiefwerder Wiesen",
+  category: "Nature",
+  address: "13597 Berlin",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686659428/SafeTravels%20app/Images/Category/Nature/Berlin_nature/Berlin_Nature_1_otlahs.png"
+)
+
+Activity.create!(
+  city_id: "1",
+  title: "Nature Park Schöneberger Südgelände",
+  category: "Nature",
+  address: "Prellerweg 47-49, 12157 Berlin",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686659427/SafeTravels%20app/Images/Category/Nature/Berlin_nature/Berlin_Nature_3_qlqfen.png"
+)
+
+Activity.create!(
+  city_id: "1",
+  title: "Landschaftspark Wuhletal",
+  category: "Nature",
+  address: "Alt-Biesdorf 46, 12683 Berlin",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686659427/SafeTravels%20app/Images/Category/Nature/Berlin_nature/Berlin_Nature_2_an7hp6.png"
+)
+
 Activity.create!(
   city_id: "1",
   title: "Brandenburg Gate",
@@ -143,10 +169,83 @@ Activity.create!(
 
 Activity.create!(
   city_id: "1",
+  title: "Berlin Cathedral",
+  category: "Culture",
+  address: "Am Lustgarten, 10178 Berlin",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686650766/SafeTravels%20app/Images/City%20images/Berlin%20Activities/Berlin_activities_4_srjzrh.png"
+)
+
+Activity.create!(
+  city_id: "1",
   title: "East Side Gallery",
   category: "Art",
   address: "East Side Gallery, Mühlenstraße 3-100, 10243 Berlin",
   image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686650767/SafeTravels%20app/Images/City%20images/Berlin%20Activities/Berlin_activities_3_twzcd2.png"
+)
+
+Activity.create!(
+  city_id: "1",
+  title: "Pergamonmuseum",
+  category: "Art",
+  address: "Bodestraße 1-3, 10178 Berlin",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686660806/SafeTravels%20app/Images/Category/Art/Berlin_art/Berlin_Art_1_iyclyc.png"
+)
+
+Activity.create!(
+  city_id: "1",
+  title: "Dark Matter",
+  category: "Art",
+  address: "Köpenicker Ch 46, 10317 Berlin",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686660806/SafeTravels%20app/Images/Category/Art/Berlin_art/Berlin_Art_3_tf78dy.png"
+)
+
+Activity.create!(
+  city_id: "1",
+  title: "Curry 36",
+  category: "Food",
+  address: "Mehringdamm 36, 10961 Berlin",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686661875/SafeTravels%20app/Images/Category/Food/Berlin_food/Berlin_Food_1_lkga1g.png"
+)
+
+Activity.create!(
+  city_id: "1",
+  title: "Berliner Fernsehturm",
+  category: "Food",
+  address: "Panoramastraße 1A, 10178 Berlin",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686661875/SafeTravels%20app/Images/Category/Food/Berlin_food/Berlin_Food_2_r29f7c.png"
+)
+
+Activity.create!(
+  city_id: "1",
+  title: "BRLO Brwhouse",
+  category: "Food",
+  address: "Schöneberger Str. 16, 10963 Berlin",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686661875/SafeTravels%20app/Images/Category/Food/Berlin_food/Berlin_Food_3_c7arp1.png"
+)
+
+# NEW YORK
+Activity.create!(
+  city_id: "2",
+  title: "Central Park",
+  category: "Nature",
+  address: "Central Park West, New York, NY, USA",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686651185/SafeTravels%20app/Images/City%20images/NY%20activities/NY_activities_3_mmn9nf.png"
+)
+
+Activity.create!(
+  city_id: "2",
+  title: "Hallett Nature Sanctuary",
+  category: "Nature",
+  address: "6th Avenue &, Central Park S, New York, NY 10019, United States",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686662451/SafeTravels%20app/Images/Category/Nature/NY_nature/NY_Nature_1_wwxfi5.png"
+)
+
+Activity.create!(
+  city_id: "2",
+  title: "Mill Rock Park",
+  category: "Nature",
+  address: "East River, New York, NY 10128, United States",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686662704/SafeTravels%20app/Images/Category/Nature/NY_nature/NY_Nature_2_jxajgl.png"
 )
 
 Activity.create!(
@@ -168,14 +267,152 @@ Activity.create!(
 Activity.create!(
   city_id: "2",
   title: "Central Park",
-  category: "Nature",
-  address: "Central Park West, New York, NY, USA",
-  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686651185/SafeTravels%20app/Images/City%20images/NY%20activities/NY_activities_3_mmn9nf.png"
-)
-Activity.create!(
-  city_id: "1",
-  title: "Berlin Cathedral",
   category: "Culture",
-  address: "Am Lustgarten, 10178 Berlin",
-  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686650766/SafeTravels%20app/Images/City%20images/Berlin%20Activities/Berlin_activities_4_srjzrh.png"
+  address: "New York, NY, United States",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686662936/SafeTravels%20app/Images/Category/Culture/NY_culture/NY_Culture_4_y9kk8t.png"
+)
+
+Activity.create!(
+  city_id: "2",
+  title: "Museum of Ice Cream",
+  category: "Art",
+  address: "558 Broadway, New York, NY 10012, United States",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686663557/SafeTravels%20app/Images/Category/Art/NY_art/NY_Art_1_oyosma.png"
+)
+
+Activity.create!(
+  city_id: "2",
+  title: "The Metropolitan Museum of Art",
+  category: "Art",
+  address: "1000 5th Ave, New York, NY 10028, United States",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686663557/SafeTravels%20app/Images/Category/Art/NY_art/NY_Art_2_ouenin.png"
+)
+
+Activity.create!(
+  city_id: "2",
+  title: "MoMa Museum",
+  category: "Art",
+  address: "11w W 53rd St, New York, NY 10019, United States",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686663557/SafeTravels%20app/Images/Category/Art/NY_art/NY_Art_3_dwivho.png"
+)
+
+Activity.create!(
+  city_id: "2",
+  title: "Black Iron Burger",
+  category: "Food",
+  address: "245 W 38th St, New York, NY 10018, United States",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686664510/SafeTravels%20app/Images/Category/Food/NY_food/NY_Food_1_n50whu.png"
+)
+
+Activity.create!(
+  city_id: "2",
+  title: "Olive Garden",
+  category: "Food",
+  address: "2 Times Sq, New York, NY 10036, United States",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686664510/SafeTravels%20app/Images/Category/Food/NY_food/NY_Food_2_fhqikl.png"
+)
+
+Activity.create!(
+  city_id: "2",
+  title: "Sweet Rehab",
+  category: "Food",
+  address: "135 Sullivan St, New York, NY 10012, United States",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686664510/SafeTravels%20app/Images/Category/Food/NY_food/NY_Food_3_befag5.png"
+)
+
+# PORTUGAL
+Activity.create!(
+  city_id: "10",
+  title: "Amarante camping",
+  category: "Nature",
+  address: "Rua Pedro Alvellos, sn, Largo Santa M.nha 56, 4600-099 Amarante, Portugal",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686667120/SafeTravels%20app/Images/Category/Nature/Amarante_nature/Amarante_nature_1_gpitqi.png"
+)
+
+Activity.create!(
+  city_id: "10",
+  title: "Amarante Garden",
+  category: "Nature",
+  address: "4600-758 Amarante, Portugal",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686667217/SafeTravels%20app/Images/Category/Nature/Amarante_nature/Amarante_nature_2_xrfb9g.png"
+)
+
+Activity.create!(
+  city_id: "10",
+  title: "Naturpassion",
+  category: "Nature",
+  address: "rua de campesinhos n27 Lomba, 4600-663 Amarante, Portugal",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686667120/SafeTravels%20app/Images/Category/Nature/Amarante_nature/Amarante_nature_3_re5lie.png"
+)
+
+Activity.create!(
+  city_id: "10",
+  title: "Igreja do Carmo",
+  category: "Culture",
+  address: "R. do Carmo, 4050-164 Porto, Portugal",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686665600/SafeTravels%20app/Images/Category/Culture/Amarante_culture/Amarante_culture_1_wa6u5u.png"
+)
+
+Activity.create!(
+  city_id: "10",
+  title: "Trilho das Azenhas",
+  category: "Culture",
+  address: "4600-758 Amarante, Portugal",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686665601/SafeTravels%20app/Images/Category/Culture/Amarante_culture/Amarante_culture_2_l66gzp.png"
+)
+
+Activity.create!(
+  city_id: "10",
+  title: "Amarantrilhos Bikeshop",
+  category: "Culture",
+  address: "R. de Freitas Bloco C fração AB, 4600-081 Amarante, Portugal",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686665602/SafeTravels%20app/Images/Category/Culture/Amarante_culture/Amarante_culture_3_sxidc2.png"
+)
+
+Activity.create!(
+  city_id: "10",
+  title: "Museu Paroquial de Arte Sacra de Amarante",
+  category: "Art",
+  address: "4600-758 Amarante, Portugal",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686666543/SafeTravels%20app/Images/Category/Art/Amarante_art/Amarante_art_1_oybib1.png"
+)
+
+Activity.create!(
+  city_id: "10",
+  title: "Ponte do Arquinho",
+  category: "Art",
+  address: "Largo do Arquinho, 4600-758 Amarante, Portugal",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686666543/SafeTravels%20app/Images/Category/Art/Amarante_art/Amarante_art_2_gngdbg.png"
+)
+
+Activity.create!(
+  city_id: "10",
+  title: "Municipal Museum Amadeo de Souza Cardoso",
+  category: "Art",
+  address: "4600-758 Amarante, Portugal",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686666542/SafeTravels%20app/Images/Category/Art/Amarante_art/Amarante_art_3_pvvnno.png"
+)
+
+Activity.create!(
+  city_id: "10",
+  title: "O Prato",
+  category: "Food",
+  address: "Edf.Cimo de Vila,LJ CB, 4600-088 Amarante, Portugal",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686666995/SafeTravels%20app/Images/Category/Food/Amarante_food/Amarante_food_1_mrgfyv.png"
+)
+
+Activity.create!(
+  city_id: "10",
+  title: "Restaurante Residencial ESTORIL",
+  category: "Food",
+  address: "R. 31 de Janeiro 150, 4600-043 Amarante, Portugal",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686666995/SafeTravels%20app/Images/Category/Food/Amarante_food/Amarante_food_2_gp18tl.png"
+)
+
+Activity.create!(
+  city_id: "10",
+  title: "Bar do Girassol",
+  category: "Food",
+  address: "Av. Gen. Silveira 193, 4600-017 Amarante, Portugal",
+  image_url: "https://res.cloudinary.com/doxaalafc/image/upload/v1686666995/SafeTravels%20app/Images/Category/Food/Amarante_food/Amarante_food_3_yntnbg.png"
 )
